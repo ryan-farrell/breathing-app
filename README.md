52aa2b1226f4e9b1d2b3ecdd51b2877250abaf7d
Ryan <ryan.farrell44@gmail.com>
2020-06-28 18:21:12 +0100

    Update Readme.md and folder structure

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-breathing-app
+# Breathing Mindfulness App
+
+A mindfulness/breathing app but meant as simple app I can play about / experiment with. I will use this app with other technologies to build 
+and maintain it. Ultimatley the goal is to learn these other technologies through it.
+
+Technologies (Goals) to include in the build and front end:
+
+- Gulp Toolkit & Task Runner (Although overkill for this app just a basic understanding and use of Task building and SASS/LESS compiler) 
+- SASS / LESS (Take the current CSS and refactor into CSS preprocessor. I'll compile with Gulp add on to create the CSS)
+- JQuery (DOM manipulation for frontend UX)
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,46 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-  }
+const gulp      = require('gulp');
+const imagemin  = require('gulp-imagemin');
+
+/**
+ * ** 4 BASIC GULP Methods
+ * gulp.task    - Define tasks
+ * gulp.src     - Point to files to use 
+ * gulp.dest    - Points to folder to output
+ * gulp.watch   - Watch files and folder for changes 
+ */
+ 
+// exports.default = () => (
+
   
-  exports.default = defaultTask;
+// );
+
+
+ // Logs Message
+ gulp.task('message', done => {
+    console.log('Gulp is running ....')
+    done();
+ });
+
+ // Copy HTML Files to Dist folder
+ gulp.task('copyHTML', done => {
+     gulp.src('src/*.html')
+     .pipe(gulp.dest('dist'));    
+     done();
+});
+
+ // Minifys Images to Dist Folder
+ gulp.task('imageMin', done => {
+    gulp.src('src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'))
+    done();
+ });
+ 
+ // Minifys JS Files
+ gulp.task('minifyJS', done => {
+    gulp.src('src/js/*')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    done();
+ });
+ 
